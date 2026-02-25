2eab8c661affa654fa1430e348ffe00d7f7b64c5
Frederik <Frederik@schabel.com>
2026-02-25 21:28:56 +0000

    added stuff

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ countries["color"] = "white"
 countries.loc[countries["NAME"] == "France", "color"] = "lightblue"
 countries.loc[countries["NAME"] == "Brazil", "color"] = "lightblue"
 
+
 # Draw the map
 fig, ax = plt.subplots(figsize=(15, 8))
 countries.plot(ax=ax, color=countries["color"], edgecolor="gray")
@@ -18,16 +19,17 @@ countries.plot(ax=ax, color=countries["color"], edgecolor="gray")
 points = [
     (2.35, 48.85, "France 90%"),
     (-47.9, -15.7, "Brazil 30%"),
+    (-47.9, -15.7, "hi"),
 ]
 
 texts = []
 for lon, lat, label in points:
-    ax.plot(lon,lat,"gray", 3)
+    ax.plot(lon, lat, "o", color="gray", markersize=3)
     texts.append(ax.text(lon, lat, label, fontsize=9,
     bbox=dict(boxstyle="round,pad=0.3, rounding_size=0.5", facecolor="white", edgecolor="lightblue", linewidth=0.8)))
 
 # This automatically moves labels so they don't overlap
-adjust_text(texts,arrowprops=dict(arrowstyle="-", color="gray", lw=0.8))
+adjust_text(texts,arrowprops=dict(arrowstyle="-", color="gray", lw=0.8), expand_text=(2, 2))
 
 ax.axis("off")
 plt.savefig("map.png", dpi=300)
